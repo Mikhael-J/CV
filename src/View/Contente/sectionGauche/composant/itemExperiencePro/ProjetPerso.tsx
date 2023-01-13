2f65a1cac933850a6784f98311bf19b010e0728a
Mik <mikhael.jankee@mail.com>
2023-01-13 17:04:17 +0100

    up
    in progress left part

--- src/View/Contente/sectionGauche/composant/itemExperiencePro/ProjetPerso.tsx
+++ src/View/Contente/sectionGauche/composant/itemExperiencePro/ProjetPerso.tsx
@@ -3,7 +3,57 @@ import React from 'react';
 function ProjetPerso() {
     return (
         <div>
-           
+
+            <dl className="row border">
+                <dt className="col-sm-3 ">Application mobile</dt>
+                <dd className="col-sm-9">Une application responsive web qui évalue la consommation électrique d'un ménage</dd>
+
+                <dt className="col-sm-3">Technologies utilisées </dt>
+                <dd className="col-sm-9">
+                    <p>React, React Native, React Native Paper, @mui, CSS, Expo</p>
+                </dd>
+            </dl>
+            <dl className="row border">
+                <dt className="col-sm-3">Messagerie</dt>
+                <dd className="col-sm-9">Une messagerie instantané</dd>
+
+                <dt className="col-sm-3">Technologies utilisées </dt>
+                <dd className="col-sm-9">
+                    <p>
+                        TypeScript, REACT, HTML/CSS, Bootstrap 5, Websocket (express),
+                        CI (CircleCI), CD (Herokuapp), Webpack, Github
+                    </p>
+                </dd>
+            </dl>
+            <dl className="row border">
+                <dt className="col-sm-3">Problème d'optimisation</dt>
+                <dd className="col-sm-9">Résolution de problème d'optimisation à l'aide de l'algorithme backtracking</dd>
+
+                <dt className="col-sm-3">Problèmes</dt>
+                <dd className="col-sm-9">
+                    <p>
+                        Peaceable Armies of Queens [1999, Robert Bosch], Séquence magique
+                    </p>
+                </dd>
+                <dt className="col-sm-3">Technologies utilisées</dt>
+                <dd className="col-sm-9">
+                    <p>
+                        Python 3.10, Makefile, Github
+                    </p>
+                </dd>
+            </dl>
+
+            <dl className="row border">
+                <dt className="col-sm-3">Script</dt>
+                <dd className="col-sm-9">Script traitement de fichier pour le renommage par lot</dd>
+
+                <dt className="col-sm-3">Technologies utilisées</dt>
+                <dd className="col-sm-9">
+                    <p>
+                        Python 3.10, test unitaire, Github
+                    </p>
+                </dd>
+            </dl>
         </div>
     );
 }
